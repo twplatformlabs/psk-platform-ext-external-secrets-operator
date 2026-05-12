5bc1df5dd8ff69f7848727ef485c18da7da714db
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-05-11 21:47:25 -0500

    [nc] generate Application resource definition and deploy with values to configuration repo
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

---
+++ scripts/generate_application_resource.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+cluster_role=$1
+
+external_secrets_operator_chart_version=$(jq -er .external_secrets_operator_chart_version environments/$cluster_role.json)
+argocd_namespace=$(jq -er .argocd_namespace environments/$cluster_role.json)
+
+mkdir deploy-files
+
+# update the application.yaml with the new chart version then stage the files for writing to the app-of-app config repo
+cat <<EOF > deploy-files/application.yaml
+# roles/production/external-secrets-operator/application.yaml
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: external-secrets-operator
+  namespace: $argocd_namespace
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+  annotations:
+    argocd.argoproj.io/sync-wave: "0"
+spec:
+  project: psk-aws-control-plane-configuration
+
+  sources:
+    - repoURL: https://kubernetes-sigs.github.io/external-secrets-operator/
+      chart: external-secrets-operator
+      targetRevision: $metrics_server_chart_version
+      helm:
+        valueFiles:
+          - \$config/roles/$cluster_role/external-secrets-operator/default-values.yaml
+          - \$config/roles/$cluster_role/external-secrets-operator/$cluster_role-values.yaml
+    - repoURL: https://github.com/twplatformlabs/psk-aws-control-plane-configuration
+      targetRevision: HEAD
+      ref: config
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: psk-system
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    retry:
+      limit: 5
+      backoff:
+        duration: 30s
+        factor: 2
+        maxDuration: 5m
+EOF
+
+cp deploy-templates/default-values.yaml deploy-files/default-values.yaml
+cp deploy-templates/$cluster_role-values.yaml deploy-files/$cluster_role-values.yaml
